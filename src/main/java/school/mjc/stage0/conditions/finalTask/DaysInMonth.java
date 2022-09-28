513d347493b75638913281737882bef12d017ee2
Olim <shnzrvolim@gmail.com>
2022-09-29 01:57:33 +0500

    solution

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -10,5 +10,8 @@ public class DaysInMonth {
                 default -> System.out.println("invalid date");
             }
         }
+        else {
+            System.out.println("invalid date");
+        }
     }
 }
